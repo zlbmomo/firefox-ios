f1b1930678bb4b714b3c63f77581e71cc4b0ea18
Farhan Patel <farhanpatel.17@gmail.com>
2017-10-13 15:37:06 -0700

    Bug 1405825 - Add accessibility identifiers to the Photon ActionSheet (#3300)

--- XCUITests/NoImageTests.swift
+++ XCUITests/NoImageTests.swift
@@ -4,32 +4,31 @@
 
 import XCTest
 
-class NoImageTests: BaseTestCase {
-    static let NoImageButtonIdentifier = "menu-NoImageMode"
+let NoImageButtonIdentifier = "menu-NoImageMode"
+let ContextMenuIdentifier = "Context Menu"
 
+class NoImageTests: BaseTestCase {
     private func showImages() {
         navigator.goto(BrowserTabMenu)
-        app.tables["Context Menu"].cells["menu-NoImageMode"].tap()
+        app.tables[ContextMenuIdentifier].cells[NoImageButtonIdentifier].tap()
         navigator.nowAt(BrowserTab)
     }
 
     private func hideImages() {
         navigator.goto(BrowserTabMenu)
-        app.tables["Context Menu"].cells["menu-NoImageMode"].tap()
+        app.tables[ContextMenuIdentifier].cells[NoImageButtonIdentifier].tap()
         navigator.nowAt(BrowserTab)
     }
 
     private func checkShowImages() {
         navigator.goto(BrowserTabMenu)
-        waitforExistence(app.tables["Context Menu"].cells["menu-NoImageMode"])
-        XCTAssertTrue(app.tables["Context Menu"].cells["menu-NoImageMode"].staticTexts["Show Images"].exists)
+        waitforExistence(app.tables.cells[NoImageButtonIdentifier])
         navigator.goto(BrowserTab)
     }
 
     private func checkHideImages() {
         navigator.goto(BrowserTabMenu)
-        waitforExistence(app.tables["Context Menu"].cells["menu-NoImageMode"])
-        XCTAssertTrue(app.tables["Context Menu"].cells["menu-NoImageMode"].staticTexts["Hide Images"].exists)
+        waitforExistence(app.tables.cells[NoImageButtonIdentifier])
         navigator.goto(BrowserTab)
     }
 
